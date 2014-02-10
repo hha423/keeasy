ea602fc0e6ff49ebf8244be42fc1cef9ad37adfc
hha423 <4168628@gmail.com>
2014-02-10 18:13:05 +0800

    Ev

---
+++ Eyesight/src/com/evan/eyesight/adapter/MainAdapter.java
@@ -0,0 +1,34 @@
+package com.evan.eyesight.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MainAdapter extends BaseAdapter {
+	
+	
+
+	public MainAdapter() {
+	}
+
+	@Override
+	public int getCount() {
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		return 0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		return arg1;
+	}
+
+}
---
+++ Eyesight/src/com/evan/eyesight/setting/SetConfig.java
@@ -0,0 +1,11 @@
+package com.evan.eyesight.setting;
+
+/**
+ * 软件功能配置类
+ * @author Evan
+ *
+ */
+public class SetConfig {
+
+	
+}
