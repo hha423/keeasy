c344e14304414655cc72f1138a469aa938504a0c
hha423 <4168628@gmail.com>
2014-02-12 17:53:10 +0800

    Evan

--- Eyesight/src/com/evan/eyesight/YanbaoActivity.java
+++ Eyesight/src/com/evan/eyesight/YanbaoActivity.java
@@ -1,45 +1,139 @@
 package com.evan.eyesight;
 
+import java.io.IOException;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class YanbaoActivity extends BaseActivity {
+
+	private RelativeLayout button_play;
+	private ImageView playsate;
+	private boolean hasm;
+	private boolean isplay;
+	private boolean flag;
+	private MediaPlayer m;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		setContentView(R.layout.yanbao_main);
 		super.onCreate(savedInstanceState);
+		m = new MediaPlayer();
+		regest();
+		isplay = false;
+		hasm = false;
 	}
 
 	@Override
 	void initView() {
 		super.initView();
+		playsate = (ImageView) findViewById(R.id.image_paly_state);
+		button_play = (RelativeLayout) findViewById(R.id.button_play);
 	}
 
 	@Override
 	void initData() {
 		text.setText("眼保健操");
+		playsate.setBackgroundResource(R.drawable.play);
 	}
 
 	@Override
 	void initListen() {
 		super.initListen();
+		button_play.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		super.onClick(v);
+		switch (v.getId()) {
+		case R.id.button_play:
+			if (isplay) {
+				isplay = false;
+			} else {
+				isplay = true;
+			}
+			initButtonPlay();
+			break;
+		}
+	}
+
+	private void initButtonPlay() {
+		if (isplay) {
+			playsate.setBackgroundResource(R.drawable.pause);
+			if (hasm) {
+				m.start();
+			} else {
+				play();
+				hasm = true;
+			}
+		} else {
+			playsate.setBackgroundResource(R.drawable.play);
+			m.pause();
+		}
+	}
+
+	private void play() {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				m = MediaPlayer.create(YanbaoActivity.this, R.raw.yanbao);
+				try {
+					m.prepare();
+				} catch (IllegalStateException illegalstateexception) {
+					illegalstateexception.printStackTrace();
+				} catch (IOException ioexception) {
+					ioexception.printStackTrace();
+				}
+				m.start();
+				m.setVolume(10F, 0.0F);
+				flag = true;
+				while (flag) {
+					System.out.println("当时间：" + m.getCurrentPosition());
+					if (m.getDuration() - m.getCurrentPosition() < 1000) {
+						// playsate.setBackgroundResource(R.drawable.play);
+						m.pause();
+						regest();
+						isplay = false;
+						hasm = false;
+						flag = false;
+						return;
+					}
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+
+	private void regest() {
+		if (m != null) {
+			m.stop();
+			m.release();
+		}
 	}
 
 	@Override
 	protected void onDestroy() {
+		regest();
 		super.onDestroy();
 	}
 
 	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		return super.onKeyDown(keyCode, event);
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
 	}
 
 }
