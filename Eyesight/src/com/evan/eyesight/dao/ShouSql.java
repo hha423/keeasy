3f565e969baa8e8963c17d3705a176c78195c49f
hha423 <4168628@gmail.com>
2014-02-14 10:38:23 +0800

    20140214

---
+++ Eyesight/src/com/evan/eyesight/dao/ShouSql.java
@@ -0,0 +1,53 @@
+package com.evan.eyesight.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ShouSql extends SQLiteOpenHelper {
+
+	public ShouSql(Context context) {
+		super(context, "coll.db", null, 2);
+	}
+
+	public void close() {
+		if (db != null)
+			db.close();
+	}
+
+	public void del(int i) {
+		if (db == null)
+			db = getWritableDatabase();
+		SQLiteDatabase sqlitedatabase = db;
+		String as[] = new String[1];
+		as[0] = String.valueOf(i);
+		sqlitedatabase.delete("CollTbl", "_id=?", as);
+	}
+
+	public void insert(ContentValues contentvalues) {
+		SQLiteDatabase sqlitedatabase = getWritableDatabase();
+		sqlitedatabase.insert("CollTbl", null, contentvalues);
+		sqlitedatabase.close();
+	}
+
+	public void onCreate(SQLiteDatabase sqlitedatabase) {
+		db = sqlitedatabase;
+		sqlitedatabase
+				.execSQL(" create table  CollTbl(_id integer primary key autoincrement,leixing text,time text, str1 text,str2 text) ");
+	}
+
+	public void onUpgrade(SQLiteDatabase sqlitedatabase, int i, int j) {
+	}
+
+	public Cursor query() {
+		return getWritableDatabase().query("CollTbl", null, null, null, null,
+				null, null);
+	}
+
+	private static final String CREATE_TBL = " create table  CollTbl(_id integer primary key autoincrement,leixing text,time text, str1 text,str2 text) ";
+	private static final String DB_NAME = "coll.db";
+	private static final String TBL_NAME = "CollTbl";
+	private SQLiteDatabase db;
+}
